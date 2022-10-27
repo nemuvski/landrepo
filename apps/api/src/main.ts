54a86c036d39725bb43f913fe96374a75d7d0681
K.Utsunomiya <kii.utsunomiya@gmail.com>
2022-10-27 23:10:54 +0900

    [apps/api] CORS有効化のメモ

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from '$/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  // TODO: 必要に応じて有効にすること
+  // app.enableCors()
   await app.listen(4000)
 }
 
