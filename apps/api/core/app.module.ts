53b8b2a978c4619d988b1918211ab966ccae6611
K.Utsunomiya <kii.utsunomiya@gmail.com>
2022-10-29 23:07:24 +0900

    ディレクトリ整理

--- apps/api/core/app.module.ts
+++ apps/api/core/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { getEnvFilePaths, validateEnv } from '$config/environments'
 import { AppController } from '$core/app.controller'
 import { AuthModule } from '$core/auth/auth.module'
+import { getEnvFilePaths, validateEnv } from '$core/common/helpers/env.helper'
 import { UsersModule } from '$core/users/users.module'
 import { UsersService } from '$core/users/users.service'
 
