6c2fcc4f1e6f1156f9a24fccc793743df9d7f24b
K.Utsunomiya <kii.utsunomiya@gmail.com>
2022-10-30 11:30:02 +0900

    環境変数追加

--- apps/api/core/common/helpers/env.helper.ts
+++ apps/api/core/common/helpers/env.helper.ts
@@ -1,3 +1,5 @@
+const MIN_LENGTH_JWT_SECRET_KEY = 32 as const
+
 export function getEnvFilePaths() {
   const paths: Array<string> = ['.env.local', '.env']
   if (process.env.NODE_ENV) {
@@ -8,10 +10,14 @@ export function getEnvFilePaths() {
 }
 
 export function validateEnv(config: Record<string, string>) {
-  const { NODE_ENV } = config
+  const { NODE_ENV, NEST_JWT_SECRET_KEY } = config
 
   if (!NODE_ENV || !['development', 'production'].includes(NODE_ENV)) {
     throw new Error('環境変数が未指定: NODE_ENV (development|production)')
+  }
+
+  if (!NEST_JWT_SECRET_KEY || NEST_JWT_SECRET_KEY.length < MIN_LENGTH_JWT_SECRET_KEY) {
+    throw new Error('環境変数が未指定または条件を満たさない: NEST_JWT_SECRET_KEY (32文字以上)')
   }
 
   return config
--- apps/api/type/global.d.ts
+++ apps/api/type/global.d.ts
@@ -4,5 +4,10 @@ declare namespace NodeJS {
      * NOTE: package.jsonのnpm-scriptsにて指定される
      */
     readonly NODE_ENV: 'production' | 'development'
+
+    /**
+     * NOTE: dotenvファイルにて指定される
+     */
+    readonly NEST_JWT_SECRET_KEY: string
   }
 }
