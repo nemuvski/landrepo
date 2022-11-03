33b959c33bd6159c33e4ef07b356c77ddc6ce44f
K.Utsunomiya <kii.utsunomiya@gmail.com>
2022-11-03 12:01:23 +0900

    next.config.jsでheadersを追加

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,30 @@
 const isProductionMode = process.env.NODE_ENV === 'production'
 const packageVersion = process.env.npm_package_version
 
+/** @type {Array<{key:string; value:string}>} */
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-Download-Options',
+    value: 'noopen',
+  },
+]
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -11,6 +35,16 @@ const nextConfig = {
   },
   env: {
     APP_VERSION: isProductionMode ? packageVersion : `${packageVersion}-${process.env.NODE_ENV}`,
+  },
+  // HTTPレスポンスヘッダの設定
+  async headers() {
+    return [
+      {
+        // アプリケーションのすべてのルートに適用
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
   },
 }
 
