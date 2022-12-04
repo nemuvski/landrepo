0f18c1d26ab393ba42db35129966ff06a314eff5
K.Utsunomiya <kii.utsunomiya@gmail.com>
2022-12-04 13:23:46 +0900

    JWTのvalidateにトークン中のsidを元にRefreshTokenテーブルにデータがあるかチェックする

--- apps/api/src/auth/jwt-refresh.strategy.ts
+++ apps/api/src/auth/jwt-refresh.strategy.ts
@@ -5,11 +5,16 @@ import { ExtractJwt, Strategy } from 'passport-jwt'
 import type { IPassportJwtStrategy } from '$/auth/interfaces/passport-strategy.interface'
 import type { JwtStrategyValidationReturnType } from '$/auth/types/strategy.type'
 import type { JwtPayload } from '@project/jwt'
+import { TokenService } from '$/auth/token.service'
 import { UsersService } from '$/users/users.service'
 
 @Injectable()
 export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh') implements IPassportJwtStrategy {
-  constructor(private configService: ConfigService, private usersService: UsersService) {
+  constructor(
+    private configService: ConfigService,
+    private usersService: UsersService,
+    private tokenService: TokenService
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -20,6 +25,12 @@ export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh'
   async validate(payload: JwtPayload): Promise<JwtStrategyValidationReturnType> {
     const user = await this.usersService.findUnique({ where: { id: payload.sub } })
     if (!user) {
+      throw new UnauthorizedException()
+    }
+    const session = await this.tokenService.findUniqueRefreshToken({
+      where: { id_userId: { id: payload.sid, userId: payload.sub } },
+    })
+    if (!session) {
       throw new UnauthorizedException()
     }
     /**
